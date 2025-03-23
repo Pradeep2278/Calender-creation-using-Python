83397fab826b82a5bc0c7b42b9f93ed92418c18d
codingPradeep <mcpradeep333@gmail.com>
2025-03-23 14:41:23 +0530

    calender creation

---
+++ calender.py
@@ -0,0 +1,12 @@
+import calendar
+
+year=int(input("Enter The Year:"))
+# month=int(input("Enter The Month:"))
+
+print("\n")
+
+# n=13
+# for i in range(1,n):
+#     print(calendar.month(year,i))
+
+print(calendar.calendar(year))
